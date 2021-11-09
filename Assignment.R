2a4b2c5657ba41bcee3f7941e42a61fe98405a6b
shah_khalid <khalid.kk704@gmail.com>
2021-11-09 20:21:06 +0500

    Assignment given to create a script file and then commit to github

---
+++ Assignment.R
@@ -0,0 +1,9 @@
+# An assignment has been given by mam Sana Rasheed to create an R script file and then commit it.
+
+name = "Shah Khalid"
+name_1 = "Abu bakr"
+
+print("The study partners of AlNafi are :")
+print(name)
+print("&")
+print(name_1)
