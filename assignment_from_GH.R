742eeb6972434dacd403c441eb39a13528c5b1ca
Shah Khalid Munir <khalid.kk704@gmail.com>
2021-11-09 20:28:30 +0500

    assignment_from_GH.R
    Another file has been added in the already existing project from the GH, and then will recieve it on the R-Studio.

---
+++ assignment_from_GH.R
@@ -0,0 +1,5 @@
+#assignment fiven by mam to create  afile from Github and then get it in the R studio
+variable_1 = "Both of the partners hae decided to make a schedule for studies"
+variable_2 = "Mr. Abubakr has followed the schedules but mr. shah khalid was a bit nasty"
+print(variable_1)
+print(variable_2)
