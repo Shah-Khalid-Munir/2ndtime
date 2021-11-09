2bb745225633bc7b0d0edf0816bab2742ae9ccf5
shah_khalid <khalid.kk704@gmail.com>
2021-11-09 20:04:05 +0500

    Few Lines Are Added at 8:03PM

--- today.r
+++ today.r
@@ -1,2 +1,9 @@
 ## This is my seconf try for repositiry
->> Print "Hello R"
+print ("Hello R")
+
+name = "Fatima Khalid"
+version = "R-Studio"
+print(name)
+print("These lines have been written in R-Studio")
+
+
